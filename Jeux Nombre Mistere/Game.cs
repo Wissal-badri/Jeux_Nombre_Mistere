eeeb825fc3a2f07cbfd39924c9397bd7edf451be
Wissal-badri <badriwiwi1@gmail.com>
2026-03-02 11:35:03 +0000

    Ajout de la limitation à 3 tentatives et indice +/- 5

--- Jeux Nombre Mistere/Game.cs
+++ Jeux Nombre Mistere/Game.cs
@@ -8,11 +8,13 @@ namespace JeuxNombreM
     {
         // Instance de la classe Random pour générer des nombres aléatoires
         private Random random = new Random();
+
         // Génère et retourne un nombre aléatoire entre min et max (inclus)
         private int GenerateRandomNumber(int min, int max)
         {
             return random.Next(min, max + 1);
         }
+
         // Demande à l'utilisateur d'entrer un nombre et le retourne sous forme d'entier (avec validation des bornes)
         private int GetUserGuess(int min, int max)
         {
@@ -26,42 +28,65 @@ namespace JeuxNombreM
             }
             return guess;
         }
+
         // Compare la proposition de l'utilisateur avec le nombre cible et affiche un message
         private bool CheckGuess(int guess, int target)
         {
-            if (guess < target)
-            {
-                Console.WriteLine("C'est plus grand !");
-                return false;
-            }
-            else if (guess > target)
-            {
-                Console.WriteLine("C'est plus petit !");
-                return false;
-            }
-            else
+            if (guess == target)
             {
                 Console.WriteLine("Bravo ! Vous avez trouvé le nombre mystère !");
                 return true;
             }
+
+            if (guess < target)
+            {
+                Console.WriteLine("C'est plus grand !");
+            }
+            else
+            {
+                Console.WriteLine("C'est plus petit !");
+            }
+
+            // Calcul de l'intervalle pour donner un indice
+            int borneInf = target - 5;
+            int borneSup = target + 5;
+            Console.WriteLine($"Vous êtes proche ! Il est entre {borneInf} et {borneSup}.");
+
+            return false;
         }
-        // Exécute la logique principale du jeu avec un nombre infini de tentatives
+
+        // Exécute la logique principale du jeu
         internal void Start()
         {
             // Définition des bornes
             int min = 1;
             int max = 100;
             
-            // Génération du nombre cible (par exemple, entre 1 et 100)
+            // Génération du nombre cible
             int target = GenerateRandomNumber(min, max);
             bool found = false;
-            // Boucle infinie jusqu'à ce que l'utilisateur trouve le nombre cible
-            // (Assure un nombre infini de tentatives)
-            while (!found)
+            
+            // Gestion du nombre de tentatives
+            int maxAttempts = 3;
+            int attempts = 0;
+
+            // Boucle jusqu'à ce que l'utilisateur trouve le nombre cible ou n'a plus de tentatives
+            while (!found && attempts < maxAttempts)
             {
+                Console.WriteLine($"--- Tentative {attempts + 1} sur {maxAttempts} ---");
                 int guess = GetUserGuess(min, max);
+                attempts++;
+                
                 found = CheckGuess(guess, target);
             }
+
+            // Si le joueur n'a pas trouvé après ses 3 tentatives
+            if (!found)
+            {
+                Console.WriteLine($"\nDommage ! Vous avez épuisé vos {maxAttempts} tentatives.");
+                Console.WriteLine($"Le nombre mystère était : {target}");
+            }
+
             Console.WriteLine("Merci d'avoir joué !");
         }
     }
